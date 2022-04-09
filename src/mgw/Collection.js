56dd014bfefc15bdaebfa5b149d8da89b7300387
Razia <nrwpc.stack@gmail.com>
2022-04-10 00:46:08 +0800

    Display No results in Collection view

--- src/mgw/Collection.js
+++ src/mgw/Collection.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Container, Divider } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import CurateFields from "./components/formgroups/CurateFields";
 import ArticleMasonry from "./components/shared/ArticleMasonry";
 import helper from "./utils/helper";
@@ -15,13 +15,18 @@ export default function Collection(props) {
       <Box sx={{ display: "flex", justifyContent: "center", pt: 5 }}>
         <CurateFields {...props} />
       </Box>
-      <Box component="section" sx={{ pt: 4, px: {xs: 3, md: 10 }}}>
+      <Box component="section" sx={{ pt: 4, px: { xs: 3, md: 10 } }}>
         {props.curatedFetched && (
           <ArticleMasonry
             {...props}
             articles={props.curatedFetched}
             type={helper.collectionView}
           />
+        )}
+        {!props.curatedFetched && (
+          <Typography component="h3" variant="h4">
+            No results found
+          </Typography>
         )}
       </Box>
     </Container>
